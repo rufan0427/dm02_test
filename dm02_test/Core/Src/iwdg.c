2d1e6de325b0f0ac5e6d591930427ceb05bd1267
yssickjgd <yssickjgd@mail.ustc.edu.cn>
2025-12-06 06:26:56 +0800

    vofa+配置文件修改更新

--- dm02_test/Core/Src/iwdg.c
+++ dm02_test/Core/Src/iwdg.c
@@ -39,7 +39,7 @@ void MX_IWDG1_Init(void)
   /* USER CODE END IWDG1_Init 1 */
   hiwdg1.Instance = IWDG1;
   hiwdg1.Init.Prescaler = IWDG_PRESCALER_256;
-  hiwdg1.Init.Window = 1024;
+  hiwdg1.Init.Window = 1023;
   hiwdg1.Init.Reload = 1023;
   if (HAL_IWDG_Init(&hiwdg1) != HAL_OK)
   {
